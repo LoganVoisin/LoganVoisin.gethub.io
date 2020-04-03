2bf66030c4a6096b2df9deee8a25ae689664e75f
Ubuntu <ubuntu@ip-172-31-85-235.ec2.internal>
2020-04-03 18:39:11 +0000

    adding in bower components

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -35,7 +35,7 @@ var init = function (window) {
 
         // TODO 3 / 7 : Call the drawCircle() function 
         
-        for (var loopsCompleted = 0; loopsCompleted < 1000; loopsCompleted++){
+        for (var loopsCompleted = 0; loopsCompleted < 100; loopsCompleted++){
             drawCircle();
         }
 
